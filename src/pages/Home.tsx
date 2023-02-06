4ae0cb9c4e3c45e494a5c846e05568bf2de028e7
Zokky2e <icro4.gaming@hotmail.com>
2023-02-06 16:29:21 +0100

    project unnecessary files cleanup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div>
+      <div>
+        <p>New Home Page</p>
+        <p>Ruap project</p>
+      </div>
+      <div>
+        <h2>Shrooms be wilding</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
